9caaec220ef13acd92196f601d5dd82d891f3cbc
Scorr <scorrdev@gmail.com>
2016-05-22 02:00:56 +0200

    added excuse to do tweening

---
+++ Assets/Scripts/UI/Hover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    [SerializeField] private float hoverTime = 1f;
+    [SerializeField] private float hoverDistance = 5f;
+
+	private void Start () {
+	    float newY = transform.position.y + hoverDistance;
+	    LeanTween.moveY(gameObject, newY, hoverTime).setLoopPingPong();
+	}
+}
